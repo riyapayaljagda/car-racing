d828bea67200bb0f668218f9fd6192bda0c08070
riya <crazy.riya.3086@gmail.com>
2021-01-01 16:57:24 +0400

    add players

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,34 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hide()
+    text("Game Start", 130,100)
+    
+    Player.getPlayerInfo()
+
+
+
+    if(allPlayers !== undefined){
+      var position = 140
+      
+      for(var plrs in allPlayers){
+
+        if(plrs === "player" + player.index){
+          fill("red")
+        }else
+        {
+          fill("black")
+        }
+
+        position=position+20
+        text(allPlayers[plrs].name +": "+ allPlayers[plrs].distance , 120,position )
+      }
+    
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 10
+      player.update() 
+    }
+  }
 }
